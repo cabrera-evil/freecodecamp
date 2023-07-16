fbe2646819d621ab8eb9fc48f94541d2c2082e9f
cabrera-evil <douglascabrera.dev@gmail.com>
2023-07-16 11:48:15 -0600

    Update: File Metadata Microservice

--- backend/boilerplate-project-filemetadata/index.js
+++ backend/boilerplate-project-filemetadata/index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+
+var multer = require('multer'); // Add multer package
 
 var app = express();
+var upload = multer({ dest: 'uploads/' }); // Specify the destination folder for uploaded files
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,10 +14,17 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// Handle file upload with multer middleware
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
 
-
+  const { originalname, mimetype, size } = req.file;
+  res.json({ name: originalname, type: mimetype, size: size });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
 });
